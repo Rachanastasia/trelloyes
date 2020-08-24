3c4acf24aa26f4905aedd5b5de931547d0d4affa
Rachel Reilly <r.a.reill18@gmail.com>
2020-08-24 16:24:51 -0500

    styling fixed

--- src/List.js
+++ src/List.js
@@ -1,5 +1,6 @@
 import Card from './Card'
 import React from 'react';
+import './List.css'
 
 // function List(props) {
 //     return (
@@ -16,7 +17,7 @@ import React from 'react';
 export default function List(props) {
     return (
         <section className='List'>
-             <header className='List-cards'><h2>{props.header}</h2></header>
+             <header className='List-header'><h2>{props.header}</h2></header>
              <div className='List-cards'>{props.cards.map(card => <Card key={card.id} title={card.title} content={card.content} />)}
              <button type="button" className="List-add-button">+ Add Random Card</button>
              </div>
